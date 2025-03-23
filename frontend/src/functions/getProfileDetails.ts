1c20626346ff1a3f6cae78a3d1be3d71f9aeed50
shuklamanan <mananshukla1512@gmail.com>
2025-03-23 20:45:06 +0530

    Refactor folder structure

--- frontend/src/functions/getProfileDetails.ts
+++
@@ -1,4 +0,0 @@
-import {executeGetApi} from "../ts/apiExecution";
-import {getProfileApi} from "./api";
-
-export const profileDetails  = (await executeGetApi(getProfileApi))[1];
--- frontend/src/functions/getUsers.ts
+++
@@ -1,5 +0,0 @@
-import {IUser} from "./interface";
-import {executeGetApi} from "../ts/apiExecution";
-import {getAllUsersApi} from "./api";
-
-export const users: IUser[] = (await executeGetApi(getAllUsersApi))[1];
