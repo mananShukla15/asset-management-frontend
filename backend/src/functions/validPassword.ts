303ebe8a6fe63dc3e9c100979f64002aca5ca1ea
mananShukla15 <mananshukla15@gmail.com>
2025-03-23 20:35:52 +0530

    Delete backend directory

--- backend/src/functions/validPassword.ts
+++
@@ -1,4 +0,0 @@
-export function isValidPassword(password: string) : boolean {
-    const regex : RegExp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,20}$/;
-    return regex.test(password);
-}
--- backend/src/routes/messages.ts
+++
@@ -1,9 +0,0 @@
-import express, {Router} from 'express';
-import {verifyJwt} from "../middleware/verifyJWT";
-import {verifyRole} from "../middleware/verifyRole";
-import {createMessage} from "../controllers/message";
-
-export const messageRoutes:Router = express.Router();
-messageRoutes.use(verifyJwt)
-// messageRoutes.get('/',getAllMessages)
-messageRoutes.post('/',verifyRole(['Admin']),createMessage)
